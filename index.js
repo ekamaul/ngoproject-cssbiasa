0c57b561bec62ab7dbd1cdad8b8e8c5ba094f320
ekamaul <ekamaher@gmail.com>
2023-06-27 17:28:54 +0700

    lanjut2

---
+++ index.js
@@ -0,0 +1,8 @@
+const boxheader = document.getElementById("boxheader")
+const boxheader2 = document.getElementById("boxheader2")
+
+function show(){
+    boxheader2.style = "display:block;"
+}
+
+addEventListener.boxheader("mouseover", show)
